ae334c690f35786db48f0d539835e0245ddd14fa
Kevin Sheedy <kevinsheedy@gmail.com>
2012-10-04 11:14:18 +0100

    First attempt at setting the caret to the correct position. Included the jquery.caret.js library

--- jquery.alphanum.js
+++ jquery.alphanum.js
@@ -1,15 +1,27 @@
+/********************************************************************
+* Limit the characters that may be entered in a text field
+* Common options: alphanumeric, alphabetic or numeric
+* Kevin Sheedy, 2012
+* http://github.com/KevinSheedy/jquery.alphanum
+*********************************************************************/
 (function( $ ){
 
-	/********************************
-	 * API goes here
-	 ********************************/
+	// API ///////////////////////////////////////////////////////////////////
 	$.fn.alphanum = function(settings) {
 		
 		var combinedSettings = getCombinedSettingsAlphaNum(settings);
+		$this = $(this);
 		this.keyup(function(){
+			var caretPos = $this.caret();
 			var input = this.value;
 			var output = trimAlphaNum(input, combinedSettings);
 			this.value = output;
+			
+			//Reset the caret position
+			if(input.length ==(output.length + 1))
+				$this.caret(caretPos - 1);
+			else
+				$this.caret(caretPos);
 		});
 		
 	};
@@ -31,6 +43,8 @@
 		});
 		
 	};
+	
+	// End of API /////////////////////////////////////////////////////////////
 	
 	function getCombinedSettingsAlphaNum(settings){
 		var userSettings, combinedSettings = {};
@@ -326,3 +340,84 @@
 		return trimNum(inputString, combinedSettings);
 	};
 })( jQuery );
+
+
+// Set caret position easily in jQuery
+// Written by and Copyright of Luke Morton, 2011
+// Licensed under MIT
+(function ($) {
+	// Behind the scenes method deals with browser
+	// idiosyncrasies and such
+	$.caretTo = function (el, index) {
+		if (el.createTextRange) { 
+			var range = el.createTextRange(); 
+			range.move("character", index); 
+			range.select(); 
+		} else if (el.selectionStart != null) { 
+			el.focus(); 
+			el.setSelectionRange(index, index); 
+		}
+	};
+	
+	// Another behind the scenes that collects the
+	// current caret position for an element
+	
+	// TODO: Get working with Opera
+	$.caretPos = function (el) {
+		if ("selection" in document) {
+			var range = el.createTextRange();
+			try {
+				range.setEndPoint("EndToStart", document.selection.createRange());
+			} catch (e) {
+				// Catch IE failure here, return 0 like
+				// other browsers
+				return 0;
+			}
+			return range.text.length;
+		} else if (el.selectionStart != null) {
+			return el.selectionStart;
+		}
+	};
+
+	// The following methods are queued under fx for more
+	// flexibility when combining with $.fn.delay() and
+	// jQuery effects.
+
+	// Set caret to a particular index
+	$.fn.caret = function (index, offset) {
+		if (typeof(index) === "undefined") {
+			return $.caretPos(this.get(0));
+		}
+		
+		return this.queue(function (next) {
+			if (isNaN(index)) {
+				var i = $(this).val().indexOf(index);
+				
+				if (offset === true) {
+					i += index.length;
+				} else if (typeof(offset) !== "undefined") {
+					i += offset;
+				}
+				
+				$.caretTo(this, i);
+			} else {
+				$.caretTo(this, index);
+			}
+			
+			next();
+		});
+	};
+
+	// Set caret to beginning of an element
+	$.fn.caretToStart = function () {
+		return this.caret(0);
+	};
+
+	// Set caret to the end of an element
+	$.fn.caretToEnd = function () {
+		return this.queue(function (next) {
+			$.caretTo(this, $(this).val().length);
+			next();
+		});
+	};
+}(jQuery));
