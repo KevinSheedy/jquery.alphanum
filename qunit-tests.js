fd00588f07670225572325432ad73a969a872681
Kevin Sheedy <kevinsheedy@gmail.com>
2013-04-30 15:05:54 +0100

    Start porting existing Unit Tests to QUnit

---
+++ qunit-tests.js
@@ -0,0 +1,67 @@
+jQuery(document).ready(function(){
+
+	var testSuiteCounter = 0;
+
+	var MASTER_TEST_DATA = getTestData();
+	
+	
+	function runTestSuite(){
+		var i = 0;
+		var testGroup;
+		for(i=0; i<MASTER_TEST_DATA.length; i++){
+			testGroup = MASTER_TEST_DATA[i];
+
+			test(testGroup.name, function(){
+				runTestGroup(testGroup);
+			});
+			
+		}
+	}
+	
+	function runTestGroup(testGroup){
+
+		var i = 0;
+		var testCase;
+		
+		for(i=0; i<testGroup.data.length; i++){
+			testCase = testGroup.data[i];
+			runTestCase(testGroup.options, testCase, testGroup.type);
+		}
+	}
+	
+	function runTestCase(options, testCase, testType){
+		
+		var input    = testCase[0];
+		var expected = testCase[1];
+		var actual;
+
+		if(testType == "alphanum")
+			runTestAlphaNum(input, expected, options);
+		else if (testType == "numeric")
+			runTestNumeric(input, expected, options);
+	}
+
+	
+	
+	function runTestAlphaNum(inputString, expectedString, options){
+		var actual = $.fn.alphanum.backdoorAlphaNum(inputString, options);
+
+		equal(actual, expectedString);
+	}
+	
+	function runTestNumeric(inputString, expectedString, options){
+		var actual = $.fn.alphanum.backdoorNumeric(inputString, options);
+
+		equal(actual, expectedString);
+	}
+	
+	runTestSuite();
+
+	
+	
+
+});
+
+test( "hello test", function() {
+	ok( 1 == "1", "Passed!" );
+});
