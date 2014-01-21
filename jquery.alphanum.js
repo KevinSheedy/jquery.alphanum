aeb1e3ae29c784deb034094a96c1772e70e65f1a
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2014-01-21 14:20:34 +0000

    Tidied up the order of the settings after adding forceUpper and forceLower

--- jquery.alphanum.js
+++ jquery.alphanum.js
@@ -50,18 +50,18 @@
 	// Start Settings ////////////////////////////////////////////////////////
 	
 	var DEFAULT_SETTINGS_ALPHANUM = {
-		allow              : '',   // Allow extra characters
-		disallow           : '',   // Disallow extra characters
-		allowSpace         : true, // Allow the space character
-		allowNumeric       : true, // Allow digits 0-9
-		allowUpper         : true, // Allow upper case characters
-		forceUpper         : false,// Convert lower case characters to upper case
-		allowLower         : true, // Allow lower case characters
-		forceLower         : false,// Convert upper case characters to lower case
-		allowCaseless      : true, // Allow characters that don't have both upper & lower variants - eg Arabic or Chinese
-		allowLatin         : true, // a-z A-Z
-		allowOtherCharSets : true, // eg é, Á, Arabic, Chinese etc
-		maxLength          : NaN   // eg Max Length
+		allow              : '',    // Allow extra characters
+		disallow           : '',    // Disallow extra characters
+		allowSpace         : true,  // Allow the space character
+		allowNumeric       : true,  // Allow digits 0-9
+		allowUpper         : true,  // Allow upper case characters
+		allowLower         : true,  // Allow lower case characters
+		allowCaseless      : true,  // Allow characters that don't have both upper & lower variants - eg Arabic or Chinese
+		allowLatin         : true,  // a-z A-Z
+		allowOtherCharSets : true,  // eg é, Á, Arabic, Chinese etc
+		forceUpper         : false, // Convert lower case characters to upper case
+		forceLower         : false, // Convert upper case characters to lower case
+		maxLength          : NaN    // eg Max Length
 	}
 	
 	var DEFAULT_SETTINGS_NUM = {
