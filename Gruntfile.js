271fe64bc7d8553453aacac8576459afc1a9120e
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2015-03-27 17:21:55 +0000

    Not using uglify just yet

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,6 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-			},
-			build: {
-				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
-			}
-		},
 		connect: {
 			server: {
 				options: {
