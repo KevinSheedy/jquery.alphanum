aed066d51e3db04bfff660e4309b745577d138b6
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2015-12-09 15:00:38 +0000

    Simple selenium usage

--- e2e/test-index.js
+++ e2e/test-index.js
@@ -1,28 +1,36 @@
 console.log('foo');
 var webdriver = require('selenium-webdriver'),
-    SeleniumServer = require('selenium-webdriver/remote').SeleniumServer
-    By = require('selenium-webdriver').By,
-    until = require('selenium-webdriver').until;
+		SeleniumServer = require('selenium-webdriver/remote').SeleniumServer
+		By = require('selenium-webdriver').By,
+		until = require('selenium-webdriver').until,
+		assert = require('assert'),
+		test = require('selenium-webdriver/testing');
 
 var pathToSeleniumJar = 'C:/apps/selenium/selenium-server-standalone-2.45.0.jar';
 var server = new SeleniumServer(pathToSeleniumJar, {
-  port: 4444
+	port: 4444
 });
 
 server.start();
 
 var driver = new webdriver.Builder().
-    usingServer(server.address()).
-    withCapabilities(webdriver.Capabilities.chrome()).
-    build();
+		usingServer(server.address()).
+		withCapabilities(webdriver.Capabilities.chrome()).
+		build();
 
-driver.get('http://localhost:08080/e2e/index.html');
-driver.findElement(By.id('textbox')).sendKeys('lorem.ipsum');
+test.describe('jquery.alphanum a textbox', function() {
+	test.it('should only allow alpha & numeric chars', function() {
+		driver.get('http://localhost:08080/e2e/index.html');
+		driver.findElement(By.id('textbox')).sendKeys('lorem.ipsum');
 
-var promise = driver.findElement(webdriver.By.id("textbox")).getAttribute('value');
+		var promise = driver.findElement(webdriver.By.id("textbox")).getAttribute('value');
 
 
-promise.then(function(text) {
-    console.log(text);
-	driver.quit();
+		promise.then(function(text) {
+			console.log(text);
+			driver.quit();
+		});
+	});
 });
+
+
