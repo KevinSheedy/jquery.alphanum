5fef04396f69720dc65cf6500abcaf65535d329f
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2015-12-10 12:23:40 +0000

    Grunt mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,17 @@ module.exports = function(grunt) {
 		},
 		watch: {
 
+		},
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec',
+					captureFile: 'results.txt', // Optionally capture the reporter output to a file 
+					quiet: false, // Optionally suppress output to standard out (defaults to false) 
+					clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false) 
+				},
+				src: ['e2e/test-textarea.js']
+			}
 		}
 	});
 
