3e2c9713b60d35edda59886533b1e2ea60df4866
Kevin Sheedy <kevinsheedy@gmail.com>
2016-03-01 16:50:52 +0000

    e2e newline

--- e2e/tests-wd.js
+++ e2e/tests-wd.js
@@ -51,5 +51,23 @@ describe('e2e tests for jquery.alphanum', function () {
 				})
 				.then(done, done);
 		});
+
+		it('newline', function (done) {
+			var browser = this.browser;
+			var textbox;
+			browser.get(e2eroot + '/newline.html')
+				.elementById('allowNewline')
+				.then(function (el) {
+					textbox = el;
+					return textbox.type('foo\nbar');
+				})
+				.then(function () {
+					return textbox.getAttribute('value');
+				})
+				.then(function (val) {
+					return assert.equal(val, 'foo\nbar');
+				})
+				.then(done, done);
+		});
 	});
 });
