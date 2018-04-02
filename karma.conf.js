0d0ee2babed291d4c2561f111ad55571d8b1604b
Kevin Sheedy <kevinsheedy@gmail.com>
2018-04-02 13:57:22 +0100

    karma sauce config

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,41 @@
 
 // See http://karma-runner.github.io/0.8/config/configuration-file.html
 module.exports = function(config) {
+
+  var customLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '35'
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '30'
+    },
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.11',
+      version: '8'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    },
+    sl_android: {
+      base: 'SauceLabs',
+      browserName: 'Browser',
+      platform: 'Android',
+      version: '4.4',
+      deviceName: 'Samsung Galaxy S3 Emulator',
+      deviceOrientation: 'portrait'
+    }
+  }
+
   config.set({
     basePath: '',
     frameworks: ['qunit'],
@@ -26,10 +61,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['sl_firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+
+    customLaunchers: customLaunchers,
   });
 };
